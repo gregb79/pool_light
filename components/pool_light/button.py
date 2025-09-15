bf930d8566ed9023a4af0b63d7e72a3663f6f3b8
gregb79 <gregbassett@bigpond.com>
2025-09-15 16:22:43 +1000

    Update button.py

--- components/pool_light/button.py
+++ components/pool_light/button.py
@@ -1,30 +1,65 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button, cc1101
-from esphome.const import CONF_ID, CONF_NAME
+from esphome.const import CONF_ID
 
-POOL_LIGHT_NS = cg.esphome_ns.namespace('pool_light')
-PoolLightButton = POOL_LIGHT_NS.class_('PoolLightButton', button.Button, cg.Component)
+DEPENDENCIES = ['cc1101']
 
-CONFIG_SCHEMA = button.button_schema(  # base button schema
-    cv.Schema({
-        cv.Required(CONF_ID): cv.declare_id(PoolLightButton),
-        cv.Required(CONF_NAME): cv.string,
-        cv.Required('id0'): cv.uint8_t,
-        cv.Required('id1'): cv.uint8_t,
-        cv.Required('instruction'): cv.string,
-        cv.Required('mode'): cv.string,
-        cv.Required('cc1101_module'): cv.use_id(cc1101.CC1101Component),
-    })
-).extend(cv.COMPONENT_SCHEMA)
+pool_light_ns = cg.esphome_ns.namespace('pool_light')
+PoolLight = pool_light_ns.class_('PoolLight', button.Button, cg.Component)
+
+CONF_ID0 = 'id0'
+CONF_ID1 = 'id1'
+CONF_INSTRUCTION = 'instruction'
+CONF_MODE = 'mode'
+CONF_CC1101_MODULE = 'cc1101_module'
+
+INSTRUCTION_VALUES = {
+    "power": 17,
+    "blue": 33,
+    "magenta": 34,
+    "red": 35,
+    "lime": 36,
+    "green": 37,
+    "aqua": 38,
+    "white": 39,
+    "mode1": 49,
+    "mode2": 50,
+    "mode3": 51,
+    "mode4": 52,
+    "brightness": 65,
+}
+
+MODE_VALUES = {
+    "pool": 1,
+    "spa": 2,
+    "poolandspa": 3,
+}
+
+CONFIG_SCHEMA = button.BUTTON_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(PoolLight),
+    cv.Optional(CONF_ID0, default=0xF9): cv.int_range(min=0x00, max=0xFF),
+    cv.Optional(CONF_ID1, default=0xCB): cv.int_range(min=0x00, max=0xFF),
+    cv.Optional(CONF_INSTRUCTION, default='power'): cv.one_of(*INSTRUCTION_VALUES.keys(), lower=True),
+    cv.Optional(CONF_MODE, default='poolandspa'): cv.one_of(*MODE_VALUES.keys(), lower=True),
+    cv.Required(CONF_CC1101_MODULE): cv.use_id(cc1101.CC1101Component),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_NAME])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await button.register_button(var, config)
-    cg.add(var.set_id0(config['id0']))
-    cg.add(var.set_id1(config['id1']))
-    cg.add(var.set_instruction(config['instruction']))
-    cg.add(var.set_mode(config['mode']))
-    cc = await cg.get_variable(config['cc1101_module'])
-    cg.add(var.set_cc1101_module(cc))
+
+    id0 = config.get(CONF_ID0, 0xF9)
+    id1 = config.get(CONF_ID1, 0xCB)
+    instruction_str = config.get(CONF_INSTRUCTION, 'power').lower()
+    mode_str = config.get(CONF_MODE, 'poolandspa').lower()
+
+    instruction = INSTRUCTION_VALUES[instruction_str]
+    mode = MODE_VALUES[mode_str]
+
+    cg.add(var.set_id0(id0))
+    cg.add(var.set_id1(id1))
+    cg.add(var.set_instruction(instruction))
+    cg.add(var.set_mode(mode))
+    cg.add(var.set_cc1101_module(config[CONF_CC1101_MODULE]))
